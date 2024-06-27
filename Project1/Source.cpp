6c6ad7ef933705c23761ba2b8fe4cfae8d9a85b5
Sasha Semeniuk <sasha.sos.06032009@gmail.com>
2024-06-27 18:05:07 +0300

    task1

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -3,9 +3,27 @@
 
 using namespace std;
 
+bool func(char* str)
+{
+	int size = strlen(str);
+	int n = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (n < 0) return false;
+		if (str[i] == '(') n++;
+		else if (str[i] == ')') n--;
+	}
+	if (n == 0) return true;
+	return false;
+}
+
 int main()
 {
+	char str[80];
+	cin.getline(str, 80);
 
+	cout << func(str);
 
 	return 0;
 }
